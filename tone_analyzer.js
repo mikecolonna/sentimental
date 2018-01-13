b201c587a286bd0e02bb867de53a8d1d5826c67f
Ana Zeneli <anazeneli@Anas-MacBook-Pro.local>
2018-01-13 13:40:03 -0500

    Updated tone_analyzer

--- tone_analyzer.js
+++ tone_analyzer.js
@@ -1,15 +1,28 @@
-//start.js
 var spawn = require('child_process').spawn,
     py    = spawn('python', ['tweet_puller.py']),
     data = [],
-    dataString = '';
+    dataString = '',
+    username = '',
+    location   = '',
+    tweets     ='',
+    tones = [];
 
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
-
 py.stdout.on('data', function(data){
-
   dataString += data.toString();
+});
+
+py.stdout.on('end', function(){
+
+  parsed_data = JSON.parse(dataString)
+
+  username = parsed_data.username
+  location = parsed_data.location
+
+  console.log('User Info:')
+  console.log(`Username: ${username} \nLocation: ${location}\n`);
+
 });
 
 var tone_analyzer = new ToneAnalyzerV3({
@@ -24,14 +37,32 @@ var params = {
   'content_type': 'application/json'
 };
 
-
 tone_analyzer.tone(params, function(error, response) {
   if (error)
     console.log('error:', error);
   else
-    console.log(JSON.stringify(response, null, 2));
-  }
-);
+    console.log("Overall tone: ")
+    var response_tones = response.document_tone.tones
+    // tones.push(response_tones)
 
-py.stdin.write(JSON.stringify(data));
-py.stdin.end();
+    for(i of response.document_tone.tones){
+        tones[i.tone_name] =  i.score
+    }
+      // console.log(i.tone_name, i.score)
+    // console.log(JSON.stringify(response, null, 2))
+    console.log(tones)
+  }
+)
+
+// .then(response => {
+//   tone = JSON.stringify(response, null, 2)
+//   console.log("WHATS THIS  DO ", tone  )
+// })
+
+// console.log("WHATS THIS  DO ", tone  )
+// console.log("WHATS THIS  DO ", tone  )
+//
+// console.log("NOTHING")
+//
+// py.stdin.write(JSON.stringify(data));
+// py.stdin.end();
--- tweet_puller.py
+++ tweet_puller.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 11 16:40:16 2018
-@author: vmoody
-"""
 import tweepy # replace all of these
+import numpy as np
+import json
+
 consumer_key = "1Q29XiMI0xvPfNrVEdxrDkieb"
 consumer_secret = "6Z4lLiNUOGdCkr1c37m7ZaVdZcfoGnbq8NiemjorKDgIcvw040"
 access_token = "811323013-OS2XFILfROSixWVlz761tCFS6rjlXORU45IHVUeQ"
 access_token_secret = "VD0MWHzaQNFCtpFzZpB1wngMCr2I6Gr2de1H6UmngYSzf"
-
 
 ####### PART 1: YOUR TIMELINE
 # Creating the authentication object
@@ -19,21 +15,12 @@ auth.set_access_token(access_token, access_token_secret)
  # Creating the API object while passing in auth information
 api = tweepy.API(auth)
 
- # Using the API object to get tweets from your timeline, and storing it in a variable called public_tweets
-"""public_tweets = api.home_timeline()
- # foreach through all tweets pulled
-for tweet in public_tweets:
-    # printing the text stored inside the tweet object
-    print(tweet.text)
-    print(tweet.created_at)
-    print(tweet.user.screen_name)
-    print(tweet.user.location)
-"""
 ##### PART 2: TWEETS FROM A SPECIFIC USER
 # Creating the API object while passing in auth information
 api = tweepy.API(auth)
 
  #The Twitter user who we want to get tweets from
+ # REPLACE WITH ON-CLICK COMMAND FRPM
 name = "joewilliams_tew"
  # Number of tweets to pull
 tweetCount = 100
@@ -41,47 +28,24 @@ tweetCount = 100
 results = api.user_timeline(id=name, count=tweetCount)
 tweets = []
 
-import numpy as np
-import json
-
-#
-# def main():
-#     print tweet.user.screen_name,
-#     tweet.user.location, tweets
  # foreach through all tweets pulled
 for tweet in results:
-    # printing the text stored inside the tweet object
     tweets = np.append(tweets, tweet.text)
 
 tweetsJSON = ' '.join(tweets)
-print tweetsJSON
+
+info = {}
+info["username"] = tweet.user.screen_name
+info["location"] = tweet.user.location
+
+print json.dumps(info)
 
 data = {}
-data ["text"] = tweetsJSON
+
+data["text"] = tweetsJSON
+
 
 file_path = "tone.json" ## your path variable
 
 with open(file_path, 'w') as outfile:
-    json.dump(data, outfile)
-
-
-
-"""
-#### PART 3: TWEETS BASED ON KEY WORDS
-#Creating the API object while passing in auth information
-api = tweepy.API(auth)
-
-# The search term you want to find
-query = "victim of domestic abuse"
-# Language code (follows ISO 639-1 standards)
-language = "en"
-
-# Calling the user_timeline function with our parameters
-results = api.search(q=query, lang=language)
-
-# foreach through all tweets pulled
-for tweet in results:
-   # printing the text stored inside the tweet object
-   print(tweet.text)
-
- """
+    json.dump(data , outfile)
